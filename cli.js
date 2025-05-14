b9d4950a2bcde0d2a86ca270a1a8c1a2b3469055
Alex Moon <moonmeister@users.noreply.github.com>
2025-05-14 15:43:28 -0700

    fix: not env vars on log commands

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 import { Option, Command } from "commander";
 import { GRAPHQL_PATH } from "./shared/constants.js";
-import packageInfo from "./package.json" with { type: "json"};
+import packageInfo from "./package.json" with { type: "json" };
 import infoAction from "./actions/info.js";
 import logAction from "./actions/log.js";
 import { parseUrl, parseInt } from "./shared/parsers.js";
@@ -9,33 +9,54 @@ const program = new Command(packageInfo.name)
 	.version(packageInfo.version)
 	.description("WordPress GraphQL Smart Cache Test for WPEngine Managed WordPress Hosting.")
 	.addOption(
-		new Option("-u, --url <url>", "WordPress URL").argParser(parseUrl).env("WORDPRESS_URL").makeOptionMandatory())
-	.addOption(
-		new Option("-g, --graphql-path <endpoint>", "GraphQL URI").default(GRAPHQL_PATH).env("GRAPHQL_PATH")
+		new Option("-u, --url <url>", "WordPress URL")
+			.argParser(parseUrl)
+			.env("WORDPRESS_URL")
+			.makeOptionMandatory(),
 	)
-	.addCommand(new Command("log")
-		.description("Logs the current caching configuration of your WPGraphQL endpoint over time.")
-		.option("-r, --run-time <seconds>", "Run time in seconds", (value) => {
-			const num = parseInt(value);
-			if (num <= 0) {
-				return Infinity;
-			}
-			return num
-		}, 60)
-		.option("-i, --interval <seconds>", "Interval between requests in seconds", (value) => {
-			const num = parseInt(value);
-			if (num < 1) {
-				return 1;
-			}
-			return num
-		}, 5)
-		.option("-l, --log-folder <folder>", "Log folder path", "./logs")
-		.action(logAction))
-	.addCommand(new Command("info")
-		.description("Provides info on the current caching configuration of your WPGraphQL endpoint.")
-		.action(infoAction)
-)
-
+	.addOption(
+		new Option("-g, --graphql-path <endpoint>", "GraphQL URI")
+			.default(GRAPHQL_PATH)
+			.env("GRAPHQL_PATH"),
+	)
+	.addCommand(
+		new Command("log")
+			.description("Logs the current caching configuration of your WPGraphQL endpoint over time.")
+			.addOption(
+				new Option("-r, --run-time <seconds>", "Run time in seconds")
+					.argParser((value) => {
+						const num = parseInt(value);
+						if (num <= 0) {
+							return Infinity;
+						}
+						return num;
+					})
+					.default(60)
+					.env("RUN_TIME"),
+			)
+			.addOption(
+				new Option("-i, --interval <seconds>", "Interval between requests in seconds")
+					.argParser((value) => {
+						const num = parseInt(value);
+						if (num < 1) {
+							return 1;
+						}
+						return num;
+					})
+					.default(5)
+					.env("INTERVAL"),
+			)
+			.addOption(
+				new Option("-l, --log-folder <folder>", "Log folder path")
+					.default("./logs")
+					.env("LOG_FOLDER"),
+			)
+			.action(logAction),
+	)
+	.addCommand(
+		new Command("info")
+			.description("Provides info on the current caching configuration of your WPGraphQL endpoint.")
+			.action(infoAction),
+	);
 
 program.parseAsync();
-
